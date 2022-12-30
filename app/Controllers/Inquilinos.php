3a8eb486cfdea0dcc3c7b3f8d251cc8f15cf01d3
lvie1509 <103891728+lvie1509@users.noreply.github.com>
2022-12-30 08:26:56 -0300

    teste

--- app/Controllers/Inquilinos.php
+++ app/Controllers/Inquilinos.php
@@ -2,7 +2,7 @@
 
 namespace App\Controllers;
 
-class Inquilinos extends MyController
+class Inquilinos extends BaseController
 {
     public function __construct(){
         $this->inquilinosModel = new \App\Models\InquilinosModel();
@@ -17,23 +17,65 @@ class Inquilinos extends MyController
     }
 
     public function recuperainquilinos(){
-        //if (!$this->request->isAJAX()){
-         //   return redirect()->back();
-        //}
+        if (!$this->request->isAJAX()){
+            return redirect()->back();
+        }
 
         $colunas = [
             "id",
             "nome",
             "email",
-            "situacao",
+            "ativo",
             "imagem",
         ];
-        $usuarios = $this->inquilinosModel->select($colunas)
+
+        $inquilinos = $this->inquilinosModel->select($colunas)
                                           ->findAll();
-                                            dd($usuarios);
+        
+        
+        //Receberá array formatado para aJax
+        $data = [];
+
+        
+
+        foreach($inquilinos as $inquilino){
+            $nomeUsuario = esc($inquilino->nome);
+
+            $data[] = array(
+                'nome' => anchor("inquilinos/exibir/$inquilino->id", esc($inquilino->nome), 'title="Editar usuário"'),
+                'email' => esc($inquilino->email),
+                'ativo' => ($inquilino->ativo == true ? 'Ativo' : '<span class="text-warning">Inativo</span>'),
+            );
+        }
+
+        $retorno = [
+            'data' => $data,
+        ];
+        // echo '<pre>';
+        // print_r($retorno);
+        // exit;
+        return $this->response->setJSON($retorno);
     }
 
-    public function cadastrar(){
-        return view('inquilinos/cadastrar');
+    public function exibir(int $id = null){
+        $inquilino = $this->buscaUsuarioOu404($id);
+
+        $data = [
+            'titulo' => "Detalhes inquilino ". esc($inquilino->nome),
+            'inquilino' => $inquilino,
+        ];
+
+        //dd($inquilino);
+        return view('Usuarios/exibir', $data);
+    }
+
+    private function buscaUsuarioOu404(int $id = null){
+
+        if(! $id || !$inquilino = $this->inquilinosModel->withDeleted(true)->find($id)){
+            throw \CodeIgniter\Exceptions\PageNotFoundException::forPageNotFound("Não foi encontrado o inquilino $id");
+        }
+
+        return $inquilino;
+
     }
 }
